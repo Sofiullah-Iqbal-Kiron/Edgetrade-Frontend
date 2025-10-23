da1247f7b1a5d0b4d40273281fefb8daeb5d0471
sajid3366 <ahteshamsajid6@gmail.com>
2025-10-23 16:02:17 +0600

    now fix

--- app/dashboard/(layout-md)/trade/page.tsx
+++ app/dashboard/(layout-md)/trade/page.tsx
@@ -11,16 +11,16 @@ export default function TradePage() {
   const [tab, setTab] = useState<'Trade' | 'Order'>('Trade')
 
   return (
-    <div className="w-full px-4">
+    <div className="w-full px-4 bg-[#DDE9FC] rounded-[10px] py-8 mt-[30px]">
       {/* Toggle */}
       <div className="flex gap-x-3 mb-3 bg-[#DDE9FC] p-2 rounded-[10px] absolute top-[227px] left-2 right-2">
         {['Trade', 'Order'].map(t => (
           <button
             key={t}
             onClick={() => setTab(t as 'Trade' | 'Order')}
-            className={`flex-1 py-4 rounded-lg text-sm font-semibold transition-all duration-300 ease-in-out transform ${
+            className={`flex-1 py-4 rounded-lg text-sm font-semibold ${
               tab === t
-                ? 'bg-blue-600 text-white shadow-md scale-[1.03]'
+                ? 'bg-blue-600 text-white shadow-md'
                 : 'bg-gray-100 text-gray-700 '
             }`}
           >
